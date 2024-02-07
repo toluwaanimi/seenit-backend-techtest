4e37606f07ca540ee4e74cce52e50cac8af567a6
Emmanuel ADEBAYO <emmanueltolu.adebayo@gmail.com>
2024-02-07 17:12:03 +0000

    chore: added support for class validator

--- packages/project/src/main.ts
+++ packages/project/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envConfig } from './infrastructure/config/environment-config/environment.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(envConfig.getPort() || 3002);
 }
 bootstrap().then();
--- packages/user/src/main.ts
+++ packages/user/src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envConfig } from './infrastructure/config/environment-config/environment.config';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   app.enableCors();
   await app.listen(envConfig.getPort() || 3000);
 }
